fbaf7777b59734bcdec8d7b800e033c99bfe015a
Nathan Thompson <thompn18@email.franklin.edu>
2024-12-07 13:25:18 -0500

    Add instructions and cpu methods

--- cpu-emulator/emulator.c
+++ cpu-emulator/emulator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <assert.h>
+
 // Memory
 // Storage: stores uint16 values in addresses 0-65535 or 0x0000-0xFFFF (65 kilobytes)
 // Operations: write a value to a memory address, read a value from a memory address
@@ -32,6 +35,85 @@ void memory_write(memory_addr addr, memory_data data) {
 memory_data memory_read(memory_addr addr) {
 	assert(addr < MEMORY_MAX);
 	return memory[addr];
+}
+
+// Instructions
+// Function: each instruction is a task that the cpu knows how to execute
+// Definition: an instruction contains a 4 bit number as the opcode and 12 bits for parameters
+// Opcodes: values 0-15 (4 bits wide)
+
+typedef struct {
+	uint16_t opcode : 4;
+	uint16_t parameters : 12;
+} instruction;
+
+enum opcodes {
+	
+						// Memory I/O - <opcode> <reg> <value/address>
+	OPCODE_LOAD,		// Operation to load data into a register from memory
+	OPCODE_SAVE,		// Operation to save data into memory from register
+
+						// Arithmetic - <opcode> <reg> <reg> <reg/store>
+	OPCODE_ADD,			// Operation to add two values together
+	OPCODE_SUB,			// Operation to subtract two values together
+	OPCODE_MUL,			// Operation to multiply two values together
+	OPCODE_DIV,			// Operation to divide two values together
+	 
+						// End - opcode
+	OPCODE_HALT			// Operation to stop execution
+};
+
+// Registers
+// Function: stores temporary and neccisary data to use in computation
+// Organization: three general purpose registers (A, B, C) with values 0-3
+
+typedef uint16_t register_val;
+
+enum registers {
+	REGISTER_A,
+	REGISTER_B,
+	REGISTER_C,
+	REGISTER_COUNT
+};
+
+
+// CPU
+// Organization: contains registers, flags
+// Operations: fetch an intstruction from memory, execute that 
+
+struct {
+	// General purpose registers A, B, and C
+	register_val registers[REGISTER_COUNT];
+
+	// Flags
+	// Function: provides information about the most recent calculation
+	// Organization: a flag should be a 4 bit number
+	bool flag_positive, flag_zero, flag_negative;
+
+	// Program counter
+	// Function: points to the next instruction to execute
+	memory_addr program_counter;
+
+	// Stack pointer??
+} cpu;
+
+void cpu_init() {
+	cpu.registers[REGISTER_A] = cpu.registers[REGISTER_B] = cpu.registers[REGISTER_C] = 0;
+
+	cpu.program_counter = 0;
+
+	cpu.flag_positive = cpu.flag_negative = false;
+	cpu.flag_zero = true;
+
+	printf("CPU Initialized.\n");
+}
+
+void cpu_fetch() {
+
+}
+
+void cpu_execute() {
+
 }
 
 int main() {
